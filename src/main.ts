7bc848c29c03f96f45ed180dcb0dbb3501cb3bdf
ashkan20171 <ashkan.mataee@yahoo.com>
2025-12-04 13:39:22 +0330

    add project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 import { App } from './app/app';
+import { routes } from './app/app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule)
+  ]
+}).catch(err => console.error(err));
